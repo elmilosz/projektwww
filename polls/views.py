ea712add94aafb42bb25c8ad8bd4406be79d5cb0
Miłosz <milosz.sawosko@infotech.edu.pl>
2022-10-10 11:09:27 +0200

    fajny commit

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
-# Create your views here.
+
+def question_list(request):
+   questions = Question.objects.all()
+   return render(request, 'polls/question_list.html', {'questions': questions})
+def question_detail(request, question_id):
+   question = get_object_or_404(Question, pk=question_id)
+   return render(request, 'polls/question_detail.html', {'question': question})
+
